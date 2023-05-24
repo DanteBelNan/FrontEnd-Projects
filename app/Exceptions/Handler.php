764490aec88125e0522002aee3f2f8505d975afe
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-05-23 23:24:13 -0300

    error 404 now redirects to home blade

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -27,4 +27,14 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    //This code makes the error 404 redirect to home.index (maybe later i could add my own 404 error page)
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
+            return redirect()->route('home.index');
+        }
+
+        return parent::render($request, $exception);
+    }
 }
