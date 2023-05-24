944bcd1c45d28eeb5fecdcf1f1b869bc11687b07
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-05-23 23:22:53 -0300

    Routes and controllers created

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProjectsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('home')->namespace('home')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home.index');
+});
+
+Route::prefix('projects')->namespace('projects')->group(function(){
+    //Route::get('/PRNAME', [ProjectsController::class, 'method'])->name('projects.PRNAME');
+
 });
