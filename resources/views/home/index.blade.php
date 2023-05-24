aca1d3a53548ef14b3496118e4c64070ba833769
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-05-24 00:03:58 -0300

    Home page structure done

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,10 +4,39 @@
 
 <div class="main-body">
     <!-- Sidebar -->
-    <aside class="sidebar-right">&nbsp;</aside>
+    <aside class="sidebar-right">
+        <p> PROFILE DATA </p> <br>
+        <p> lorem ipsum </p> <br>
+        <p> lorem ipsum </p> <br>
+        <p> lorem ipsum </p> <br>
+        <p> lorem ipsum </p> <br>
+        <p> lorem ipsum </p> <br>
+        <p> lorem ipsum </p> <br>
+        <p> lorem ipsum </p> <br>
+        
+    </aside>
 
     <!-- Main -->
-    <main class="sidebar-left">&nbsp;</main>
+    <main class="sidebar-left">
+        <p> CARDS SPACE </p> <br>
+        <p> lorem ipsum </p> <br>
+        <p> lorem ipsum </p> <br>
+        <p> lorem ipsum </p> <br>
+        <p> lorem ipsum </p> <br>
+        <p> lorem ipsum </p> <br>
+        <p> lorem ipsum </p> <br>
+        <p> lorem ipsum </p> <br>
+        <p> lorem ipsum </p> <br>
+        <p> lorem ipsum </p> <br>
+        <p> lorem ipsum </p> <br>
+        <p> lorem ipsum </p> <br>
+        <p> lorem ipsum </p> <br>
+        <p> lorem ipsum </p> <br>
+        <p> lorem ipsum </p> <br>
+        <p> lorem ipsum </p> <br>
+
+        
+    </main>
 </div>
     
 @endsection
