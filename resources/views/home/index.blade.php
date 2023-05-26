29e8128bff864cfdf6300d075dd6fb6471b90b76
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-05-26 19:38:27 -0300

    fonts uploaded, images fitted (desc must be completed and profile photo too)

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,21 +6,25 @@
     <!-- Sidebar -->
     <aside class="sidebar-right">
         <div class="top-profile">
-            Background
+            <img src=" {{asset('img/profile-card/bg-pattern-card.svg')}}" alt="profile-top-img">
         </div>
         <div class="profile-photo">
-
+            <img src=" {{asset('img/profile-card/bg-pattern-top.svg')}}" alt="profile-top-img">
         </div>
         <div class="profile-data">
-            <span>Dante Beltran</span>
-            <span>26</span>
-            Buenos Aires, Argentina
-    
+            <div class="name">Dante Beltran</div>
+            <div class="age">26</div>
+            <div class="location">Buenos Aires, Argentina </div>
+            <div class="tech">
             Tecnologias
-    
+            </div>
+            <div class="hobbies">
             Pasiones
-    
+            </div>
+
+            <div class="contact">
             Contact buttons
+            </div>
         </div>
 
         
