9b031ba8693d798b5cf488d97ef4029f17d24bbc
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-05-29 00:42:20 -0300

    Js code comented and optimized

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -2,6 +2,7 @@ function toggleDropdown(dropdownToggle) {
     var dropdownMenu = dropdownToggle.nextElementSibling;
     var dropdownImg = dropdownToggle.querySelector('img');
   
+    //The next code is optimized with classes being added and removed in the next lines...
     //dropdownMenu.style.display = (dropdownMenu.style.display === "block") ? "none" : "block";
     //dropdownMenu.style.maxHeight = (dropdownMenu.style.display === "0vh") ? "fit-content" : "0vh";
 
@@ -14,6 +15,7 @@ function toggleDropdown(dropdownToggle) {
 
   }
   
+  //This function does nothing right now, but it may help me later
   function selectOption(dropdownItem, option) {
     var dropdownToggle = dropdownItem.parentElement.previousElementSibling;
     //dropdownToggle.innerHTML = "Opción " + option; //Esto reasigna el valor del texto a la variable dropdown
