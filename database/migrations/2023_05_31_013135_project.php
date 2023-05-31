b1624e919b7d99e72b02f05c97de153f55da0923
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-05-30 22:35:36 -0300

    projects migration done

---
+++ database/migrations/2023_05_31_013135_project.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('desc', 500)->nullable();
+            $table->string('framework');
+            $table->string('img')->default('defaultAvatar');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('projects');
+    }
+};
