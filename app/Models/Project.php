d7633157374dbcc97775a05627ffdeb243e18043
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-05-31 22:04:45 -0300

    model created

---
+++ app/Models/Project.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Player extends Model
+{
+    use HasFactory;
+
+    //Por defecto agarra la tabla players, pero si quisiese dejarlo fijado seria con
+    //protected $table = "player";
+    protected $fillable = [
+        'name',
+        'desc',
+        'framework',
+        'img',
+        'created_at',
+    ];
+}
