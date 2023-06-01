de462bc3cfb90fe947e562d47ba47c8e9162e6b1
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-05-31 22:13:20 -0300

    show and create methods done

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Projects;
+use App\Models\Project;
 
 class ProjectsController extends Controller
 {
     //
+    public function show($id){
+        $project = Project::findorFail($id);
+        $name = $project->name;
+        return view('projects.'.$name , [
+            'project' => $project
+        ]);
+    }
+    public function create(){
+        //redirects to form
+        return view('players.create');
+    }
+
 }
