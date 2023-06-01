725851b4691b5393a95148aa316754e3c73b7c28
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-05-31 22:23:47 -0300

    store controller created

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -19,5 +19,34 @@ class ProjectsController extends Controller
         //redirects to form
         return view('players.create');
     }
+    public function store(Request $request){
+        //Declaring the variables of the request
+        $name = $request->input('name');
+        $desc = $request->input('desc');
+        $framework = $request->input('framework');
+        $img = 0; // needs an extra function that stores the img in resources and saves the direction of the file in the variable
+        $created_at = 0; //this need to has an extra  function that detects time
 
+        //Creating the file
+        $filePath = resource_path('views/projects/' . $name . '.blade.php');
+        $fileContent = "
+        @extends('layouts.layouts')
+
+        @section('content')
+        @endsection";
+        file_put_contents($filePath, $fileContent);
+
+        //Creating de project in db
+        $project = new Project();
+        $project->name = $name;
+        $project->desc = $desc;
+        $project->framework = $framework;
+        $project->img = $img;
+        $project->created_at = $created_at;
+
+        $project->save();
+
+        return redirect()->route('projects.show', $project->id);
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,8 @@ Route::prefix('home')->namespace('home')->group(function () {
 
 Route::prefix('projects')->namespace('projects')->group(function(){
     //Route::get('/PRNAME', [ProjectsController::class, 'method'])->name('projects.PRNAME');
+    //poner el show
+    //poner el create
+    //poner el store
 
 });
