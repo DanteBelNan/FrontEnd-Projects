fd2a7683b1e1e16367658cc0ab8d00d9d0fec0b1
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-06-23 16:08:43 -0300

    template for edit form created

---
+++ resources/views/projects/edit.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.layouts')
+@section('navbar')
+
+
+<form action="{{ route('home.adminPanel')}}" method="post" id="adminPanel">
+    @csrf
+    <input type="hidden" value=true name="logged" id="logged">
+    <button type="submit" class="navbar-button">Admin Panel</button>
+</form>
+
+@endsection
+
+@section('content')
+
+
+@endsection
